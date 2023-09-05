fbe680875e06020f66f28944dd4f0865e100166f
gram <git@orsinium.dev>
2023-09-05 21:50:58 +0200

    stdlib draft

--- netlify.sh
+++ netlify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# This script is used by netlify
+# This script is used by netlify and cloudflare pages
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
 ./bin/task build
